b075a436ed17386cba0b0f5d7db5b83822e457cc
Steve Mathews <stemathe1@publicisgroupe.net>
2021-06-22 00:05:07 +0530

    spring mvc using java configurations

---
+++ springmvc/src/main/java/com/steve/spring/springmvc/controller/SpringConfig.java
@@ -0,0 +1,30 @@
+package com.steve.spring.springmvc.controller;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@EnableWebMvc
+@ComponentScan("com.steve.spring.springmvc.controller")
+@Configuration
+public class SpringConfig extends WebMvcConfigurerAdapter {
+	
+	@Bean
+	public ViewResolver viewResolver() {
+		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+		resolver.setPrefix("/WEB-INF/views/");
+		resolver.setSuffix(".jsp");
+		return resolver;
+	}
+	
+	@Override
+	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+		configurer.enable();
+	}
+
+}
